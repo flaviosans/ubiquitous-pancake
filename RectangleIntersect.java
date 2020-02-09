78dcedae0469f60e5a0fb89c51dfe3b7c22549e7
Flávio Santos <flaviosantos.dev@gmail.com>
2020-02-09 09:20:17 -0300

    It works!

--- RectangleIntersect.java
+++ RectangleIntersect.java
@@ -6,9 +6,13 @@ public class RectangleIntersect {
             new Point(7,2), new Point(13,7));
         Rectangle c = new Rectangle(
             new Point(10,10), new Point(14,13));
-        System.out.println(a.intersects(b));
-        System.out.println(a.intersects(c));
 
-        System.out.println(a.areaOfIntersection(b));
+        System.out.println("A intersects B? " + a.intersects(b));
+        System.out.println("A intersects C? " + a.intersects(c));
+        System.out.println("B intersects C? " + b.intersects(c));
+
+        System.out.println("Area of Intersection (A,B): " + a.areaOfIntersection(b));
+        System.out.println("Area of Intersection (A,C): " + a.areaOfIntersection(c));
+        System.out.println("Area of Intersection (B,C): " + b.areaOfIntersection(c));
     }
 }
